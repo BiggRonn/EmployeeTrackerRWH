3f61392007e294f7df697f8d5a95f60f03c49945
Wes Howard <BigRonald42@hotmail.com>
2021-03-30 20:25:55 -0700

    updated choices for adding to tables

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const mysql = require('mysql');
 const inquirer = require("inquirer");
-const myUtils = require("./lib/utils");
-const connection = require("./lib/connection")
+
+const util = require("util");
+
+//const {connection} = require("./lib/connection")
+// const{
+//   updateEmployeeRole,
+//   addEmployee,
+//   addRole,
+//   addDepartment,
+//   viewEmployees,
+//   viewRoles,
+//   viewDepartments
+// } = require("./lib/utils");
 
 
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'company_db',
+  });
+  
+  
+  
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+  });
+  
+  connection.query = util.promisify(connection.query);
 
 
 function init() {
@@ -24,7 +53,7 @@ function init() {
   }]).then(function (response) {
     switch (response.action) {
       case "Add Employee":
-        myUtils.addEmployee();
+        addEmployee();
         break;
       case "Add Department":
         addDepartment();
@@ -48,6 +77,150 @@ function init() {
         connection.end();
     }
   })
+}
+
+const rQuestions = [{
+  type: "input",
+  name: "title",
+  message: "What is the title of the role?"
+},
+{
+  type: "input",
+  name: "salary",
+  message: "What is the salary?"
+}, {
+  type: "list",
+  name: "department",
+  message: "What is the department?",
+  choices: [],
+}]
+
+//questions for addEmployee prompt... choices arrays to be filled with sql query
+const eQuestions = [{
+  type: "input",
+  name: "firstName",
+  message: "What is the employee's first name?"
+},
+{
+  type: "input",
+  name: "lastName",
+  message: "What is the employee's last name?"
+},
+{
+  type: "list",
+  name: "roleID",
+  message: "What is the employee's role?",
+  choices: []
+}, {
+  type: "input",
+  name: "managerID",
+  message: "Who is the employee's manager?",
+  choices: []
+}]
+
+
+
+
+
+//get this to work with employee name.
+function updateEmployeeRole() {
+  inquirer.prompt([{
+      type: "input",
+      name: "empID",
+      message: "What is the employee's ID?"
+  },
+  {
+      type: "input",
+      name: "newRole",
+      message: "What role would you like this employee to have?"
+  }]).then(function (response) {
+      const query = "UPDATE employees SET role_id = (?) WHERE id = (?)";
+
+      connection.query(query, [response.newRole, response.empID]);
+
+      init();
+  })
+
+}
+
+
+function viewEmployees() {
+  connection.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.title, department.name AS department, roles.salary, employees.first_name AS Manager
+  FROM employees INNER JOIN roles
+  ON (employees.role_id = roles.id)
+  INNER JOIN department
+  ON(roles.department_id = department.id);`, function (err, data) {
+      console.table(data);
+      init();
+  });
+}
+function viewDepartments() {
+  connection.query("SELECT * FROM departments", function (err, data) {
+      console.table(data);
+      init();
+  });
+}
+function viewRoles() {
+  connection.query("SELECT * FROM roles", function (err, data) {
+      console.table(data);
+      init();
+  });
+}
+
+async function addEmployee() {
+  const roleList = await connection.query("SELECT roles.title FROM roles;");
+  console.log(roleList.map(x => x.title));
+
+  const managerList = await connection.query("SELECT employees.first_name FROM employees;");
+  console.log(managerList);
+
+   eQuestions[2].choices = roleList.map(x => x.title);
+  eQuestions[3].choices = managerList.map(x=> x.first_name);
+
+
+  await inquirer.prompt(eQuestions).then(function (response) {
+      console.log(response);
+      const query = "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);";
+
+      const foo = connection.query(query, [response.firstName, response.lastName, response.roleID, response.managerID]) 
+          console.log("Added employee", response.lastName);
+          console.log(foo.sql);
+          init();
+      
+  })
+}
+
+async function addRole() {
+  const departmentList = await connection.query("SELECT department.name FROM department;");
+  rQuestions[2].choices = departmentList.map(x=> x.name);
+
+  inquirer.prompt(rQuestions)
+      .then(function (response) {
+          console.log(response);
+          const query = "INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?);";
+
+          const foo = connection.query(query, [response.title, response.salary, response.departmentID], function (err, data) {
+              console.log("Added role", response.title);
+              
+              init();
+          })
+      })
+}
+
+
+function addDepartment() {
+  inquirer.prompt([{
+      type: "input",
+      name: "name",
+      message: "What is the name of the department?"
+  }]).then(function (response) {
+      const query = "INSERT INTO department (name) VALUES (?);";
+      const foo = connection.query(query, [response.name]);
+      console.log("Added department", response.name);
+      init();
+  })
+
+
 }
 
 init();
--- assets/db/employeeFirst.sql
+++ assets/db/employeeFirst.sql
@@ -30,11 +30,20 @@ CREATE TABLE department(
 );
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES (Timothy, Aolee, 3, 13);
+VALUES (Timothy, Aolee, 3, 13),(BOBB, werd, 3, 3),(David, wre, 3, 1),(Greg, Azxcv, 2, 1),(Paul, gdsage, 2, 1);
 
 INSERT INTO roles(title, salary, department_id)
-VALUES (3, "Manager", 150,000, 1);
+VALUES (3, "Manager", 150000, 1),(3, "Engineer", 50000, 1),(3, "Lawyer", 250000, 1);
 
 INSERT INTO department(name)
 VALUES("Administration"),("Technical"),("Legal"),("Sales");
+
+SELECT employees.id, employees.first_name, employees.last_name, roles.title, department.name, roles.salary, employees.first_name AS Manager
+FROM employees INNER JOIN roles
+ON (employees.role_id = roles.id)
+INNER JOIN department
+ON(roles.department_id = department.id);
+
+
+
 
