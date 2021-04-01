65a3841d58b37eb3da12f5e6fe48175071162a50
Wes Howard <BigRonald42@hotmail.com>
2021-03-31 20:05:43 -0700

    updated seed sql

--- assets/db/employeeFirst.sql
+++ assets/db/employeeFirst.sql
@@ -1,8 +1,22 @@
-DROP IF EXISTS company_db;
+DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
 USE company_db;
 
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    PRIMARY KEY (id)
+
+);
+CREATE TABLE roles(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) UNIQUE NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) 
+);
 CREATE TABLE employees(
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(255),
@@ -16,30 +30,16 @@ CREATE TABLE employees(
 
 );
 
-CREATE TABLE roles(
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(255) UNIQUE NOT NULL,
-    salary DECIMAL,
-    department_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id) 
-);
-
-CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) UNIQUE NOT NULL,
-    PRIMARY KEY (id)
-
-);
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Timothy", "Aolee", 1, null),("Bob","Werd", 3, 1),("Ahmed", "Perry", 3, 1),("Greg", "Azerces", 2, 2),("Paul", "Dinsage", 2, 2);
+INSERT INTO department(name)
+VALUES("Administration"),("Technical"),("Legal"),("Sales");
 
 INSERT INTO roles(title, salary, department_id)
 VALUES ("CEO", 2550000, 1),("Engineer", 100000, 2),("Lawyer", 250000, 3),("Sales Lead", 50000, 4);
 
-INSERT INTO department(name)
-VALUES("Administration"),("Technical"),("Legal"),("Sales");
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Timothy", "Aolee", 1, null),("Bob","Werd", 3, 1),("Ahmed", "Perry", 3, 1),("Greg", "Azerces", 2, 2),("Paul", "Dinsage", 2, 2);
+
+
 
 SELECT employees.id, employees.first_name, employees.last_name, roles.title, department.name, roles.salary, employees.first_name AS Manager
 FROM employees INNER JOIN roles
